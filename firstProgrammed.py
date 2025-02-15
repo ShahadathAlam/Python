899b4fb2d934f8075cb138dd42a4779034f3b3c6
ShahadathAlam <shahadathalam@ymail.com>
2025-02-15 22:40:27 +0600

    Chapter 1 completed

--- firstProgrammed.py
+++ firstProgrammed.py
@@ -1,1 +1,22 @@
-print('my name is shahadath')
+# print('my name is shahadath')
+# price = float(input("Enter your price:"))
+# price = input("Enter your price:")
+# price = int(input("Enter your price:"))
+# print(type(price), price)
+
+# num1 = int(input("Enter the first number "))
+# num2 = int(input("Enter the second number "))
+# sum = num1 + num2
+# print(sum)
+
+# side = int(input("Enter the side of a square "))
+# area = side*side
+
+# print("The area of the square is:", area) 
+
+num1 = float(input("Enter the first number "))
+num2 = float(input("Enter the second number "))
+avg = (num1 + num2)/2
+print(avg)
+
+#Chapter 2: Strings and Conditional Statements
